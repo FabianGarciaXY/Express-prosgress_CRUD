a3debecd3433be72d44f5f2443631131a5b13af8
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-08 22:05:20 -0500

    Se crearon los endpoits para la tabla explorers

--- server.js
+++ server.js
@@ -11,6 +11,53 @@ app.get('/', (req, res) => {
   res.json({message: 'Alive'});
 });
 
+app.get('/explorers', async (req, res) => {
+  const allExplorers = await prisma.explorer.findMany({});
+  res.status(200).json({
+    allExplorers
+  });
+});
+
+app.get('/explorers/:id', async (req, res) => {
+  const id = req.params.id;
+  const explorer = prisma.explorer.findUnique({
+    where: {id: parseInt(id)}
+  });
+});
+
+app.post('/explorers', async (req, res) => {
+  const explorer {
+    name = req.body.name,
+    username = req.body.username,
+    mission = req.body.mission
+  }
+  const message = {"Explorer creado"}
+  await prisma.explorer.create({
+    data: explorer
+  });
+  res.json({message});
+});
+
+app.put('/explorers/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.explorer.update({
+    where: {
+      id: id
+    }
+    data: {
+      mission: req.body.mission;
+    }
+  });
+  return res.json({message: "Actualizado con éxito"});
+});
+
+app.delete('/expreres/:id', asyc (reqm res) => {
+  const id = req.params.id;
+  await prisma.explorer.delete({where { id: id}});
+  return res.json(message: "Eliminado correctamente");
+});
+
+
 app.listen(port, () => {
   console.log(`Listening in port ${port}`);
 });
